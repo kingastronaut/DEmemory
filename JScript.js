a197fb5aeb2dc731faa943b5b5d340ccad64a776
kingastronaut <59347076+kingastronaut@users.noreply.github.com>
2024-08-02 12:17:57 +0300

    Update JScript.js

--- JScript.js
+++ JScript.js
@@ -11,7 +11,21 @@ fetch (file)
   var lines= csv.split("\n");
   var result = [];
   var headers=lines[0].split(",");
-  console.log(lines);
+  
+  for(var i=1;i<lines.length;i++){
+
+      var obj = {};
+      var currentline=lines[i].split(",");
+
+      for(var j=0;j<headers.length;j++){
+          obj[headers[j]] = currentline[j];
+      }
+
+      result.push(obj);
+
+  }
+  console.log(result);
+  return JSON.stringify(result); //JSON
 });
 
 
