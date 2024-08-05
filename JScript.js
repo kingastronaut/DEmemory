2af4cc8b57ebb0ee21ef1e16e388db26cee5f672
kingastronaut <59347076+kingastronaut@users.noreply.github.com>
2024-08-05 21:57:59 +0300

    Update JScript.js

--- JScript.js
+++ JScript.js
@@ -9,7 +9,7 @@ function emptyConsoleP(){
 
 function flashMessage(x, y) {
   document.getElementById('consoleP').innerHTML = x;
-  setTimeout(emptyConsoleP(), y);
+  //setTimeout(emptyConsoleP(), y);
 }
 
 flashMessage("Welcome!", 3000)
